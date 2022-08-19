6f6f0ae5485aec977449bf3d4eb4ca4bd8dd8e7b
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-08-19 14:39:46 -0300

    teste

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,19 @@
+public class NaveEspacialDecorator extends NaveEspacial{
+
+    private NaveEspacial naveDecorada;
+
+    public NaveEspacialDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada.getSaude(), naveDecorada.getAtaque());
+        this.naveDecorada = naveDecorada;
+    }
+
+    @Override
+    public int getSaude() {
+        return naveDecorada.getSaude();
+    }
+
+    @Override
+    public int getAtaque() {
+        return naveDecorada.getAtaque();
+    }
+}
