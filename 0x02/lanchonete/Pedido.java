343a41002a712674890f02fc0fc095c16b889e85
Beatriz Gomes Barbosa <beatrizgb@ciandt.com>
2022-08-19 14:06:01 -0300

    teste

---
+++ 0x02/lanchonete/Pedido.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+
+public class Pedido {
+
+    private HashSet<ItemPedido> itensDentroCaixa = new HashSet<>();
+
+    private HashSet<ItemPedido> itensForaCaixa = new HashSet<>();
+
+    public void adicionarItemDentroCaixa(ItemPedido item){
+        getItensDentroCaixa().add(item);
+    }
+
+    public void adicionarItemForaCaixa(ItemPedido item){
+        getItensForaCaixa().add(item);
+    }
+
+    public HashSet<ItemPedido> getItensDentroCaixa() {
+        return itensDentroCaixa;
+    }
+
+    public void setItensDentroCaixa(HashSet<ItemPedido> itensDentroCaixa) {
+        this.itensDentroCaixa = itensDentroCaixa;
+    }
+
+    public HashSet<ItemPedido> getItensForaCaixa() {
+        return itensForaCaixa;
+    }
+
+    public void setItensForaCaixa(HashSet<ItemPedido> itensForaCaixa) {
+        this.itensForaCaixa = itensForaCaixa;
+    }
+    @Override
+    public String toString() {
+        StringBuilder itensForaDaCaixa = new StringBuilder("\tFora da Caixa:\n");
+        StringBuilder itensDentroDaCaixa = new StringBuilder("\tDentro da Caixa:\n");
+
+        getItensForaCaixa().stream().sorted(new ItemPedido()).forEach(itemPedido -> itensForaDaCaixa.append(String.format("\t\t- %s %s\n", itemPedido.getTipo(), itemPedido.getNome())));
+        getItensDentroCaixa().stream().sorted(new ItemPedido()).forEach(itemPedido -> itensDentroDaCaixa.append(String.format("\t\t- %s %s\n", itemPedido.getTipo(), itemPedido.getNome())));
+
+        return itensForaDaCaixa + itensDentroDaCaixa.toString();
+    }
+}
